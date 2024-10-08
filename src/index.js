1b8c3bdb6d2eb146bf6364b36c63d88809321c3f
unknown <amincxo@gmail.com>
2024-10-09 01:25:12 +0330

    create provider and import apollo

--- src/index.js
+++ src/index.js
@@ -2,10 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ApolloProvider , ApolloClient , InMemoryCache  } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: "https://graphqlzero.almansi.me/api",
+    cache : new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client} >
+        <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
