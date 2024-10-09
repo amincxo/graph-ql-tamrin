74f56a466abfad3de11b6f6ccf10d8d581bb0582
unknown <amincxo@gmail.com>
2024-10-09 17:55:17 +0330

    src/components/user.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
 import User from './components/User';
+import Users from './components/Users';
 
 function App() {
   return (
     <div className='App' >
-        {/* <Users/> */}
-        <User />
+        <Users/>
+        {/* <User /> */}
     </div>
 );
 }
